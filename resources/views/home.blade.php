53ae08a3b96705f8a1cda71f4844f8b5270c98d9
Manuel della Gala <manuel.dellagala@gmail.com>
2021-06-10 09:27:17 +0200

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,9 @@
     <div class="container">
         <p>
 
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis possimus recusandae neque quasi ipsa sit sint deleniti amet earum quidem aliquam autem unde asperiores distinctio assumenda soluta, delectus voluptates doloremque!
 
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe tempora error quae, iure ut amet vel, recusandae maxime quaerat delectus, cum debitis. Iste veritatis provident, quis officiis quisquam veniam nostrum?
 
         </p>
     </div>
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -6,23 +6,25 @@
 
 
 
-                @foreach ($movies as $movie)
 
-                    <div class="d-inline-flex p-2 bd-highlight justify-content-around">
-                        <div class="card shadow-sm ">
-                          <div class="card-body">
-                            <p class="card-text d-inline flex-column ">
-                                <div>Titolo: {{ $movie->title }}</div>
-                                <div>Data: {{ $movie->date }}</div>
-                                <div>Voto: {{ $movie->vote }}</div>
-                            </p>
 
-                          </div>
-                        </div>
+                    <div class="d-flex flex-wrap p-2 bd-highlight justify-content-around">
+                         @foreach ($movies as $movie)
+                            <div class="card shadow-sm my-1 ">
+                                <div class="card-body">
+                                    <p class="card-text d-inline flex-column ">
+                                        <div>Titolo: {{ $movie->title }}</div>
+                                        <div>Data: {{ $movie->date }}</div>
+                                        <div>Voto: {{ $movie->vote }}</div>
+                                    </p>
+
+                                </div>
+                            </div>
+                        @endforeach
                       </div>
 
 
-                @endforeach
+
 
 
 
